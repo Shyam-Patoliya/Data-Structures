09feb3f4ad6d8827352c71690c8a3435b11367b8
Shyam Patoliya <141806097+Shyam-Patoliya@users.noreply.github.com>
2023-08-28 10:03:14 +0530

    Update Shyam Binary Search 02.c

--- Shyam Binary Search 02.c
+++ Shyam Binary Search 02.c
@@ -3,10 +3,8 @@
 int binarySearch(int arr[], int size, int target) {
     int left = 0;
     int right = size - 1;
-
     while (left <= right) {
         int mid = left + (right - left) / 2;
-
         if (arr[mid] == target) {
             return mid; 
         } else if (arr[mid] < target) {
@@ -15,7 +13,6 @@ int binarySearch(int arr[], int size, int target) {
             right = mid - 1;
         }
     }
-
     return -1;
 }
 
@@ -23,24 +20,19 @@ int main() {
     int size;
     printf("Enter the size of the array: ");
     scanf("%d", &size);
-
     int arr[size],i;
     printf("Enter the elements of the array in ascending order:\n");
     for (i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
-
     int key;
     printf("Enter the element to search for: ");
     scanf("%d", &key);
-
     int result = binarySearch(arr, size, key);
-
     if (result != -1) {
         printf("Element found at index %d.\n", result);
     } else {
         printf("Element not found in the array.\n");
     }
-
     return 0;
 }
